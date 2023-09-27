d0a6957083b1994cbc577085ef5dbef8699226d5
Servicio de Anestesiologia Hospital Italiano de Buenos Aires <anestesiasistemas@users.noreply.github.com>
2023-09-27 11:23:16 -0300

    Update wsgi.py

--- BCCT/BCCT/wsgi.py
+++ BCCT/BCCT/wsgi.py
@@ -14,5 +14,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BCCT.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='CTPortal/static', prefix='static/')
 
