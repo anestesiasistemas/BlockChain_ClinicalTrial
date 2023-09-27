31028225f709b5764597891b2843812cde9a5873
Servicio de Anestesiologia Hospital Italiano de Buenos Aires <anestesiasistemas@users.noreply.github.com>
2023-09-27 11:40:22 -0300

    Update settings.py

--- BCCT/BCCT/settings.py
+++ BCCT/BCCT/settings.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/CTPortal/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/CTPortal'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
